acd8b3186a8b1b44a3ce53009be2899b6262770d
Somu-ed <satpathysoumyajeet.99@gmail.com>
2021-01-11 20:13:46 +0530

    showYouWon, showNumberAbove, showNumberBelow done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
- * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
+ * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
  * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
  * TODO: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
@@ -40,11 +40,11 @@ function playGame(){
 
 function displayResult(numberGuess) {
   if (numberGuess > correctNumber) {
-    console.log('Number is too high!');
+    showNumberAbove();
   } else if (numberGuess < correctNumber) {
-    console.log('Number is too low!');
+    showNumberBelow();
   } else {
-    console.log('Number is Correct!');
+    showYouWon();
   }
 }
 
@@ -128,6 +128,7 @@ function showYouWon(){
    * HINT: Use the 'won' and text parameters 
    */
   // *CODE GOES BELOW HERE *
+  let dialog = getDialog("won", text);
 
   document.getElementById("result").innerHTML = dialog;
 }
@@ -140,6 +141,7 @@ function showNumberAbove(){
    * HINT: Use the 'warning' and text parameters 
    */
   // *CODE GOES BELOW HERE *
+  let dialog = getDialog("warning", text);
 
   document.getElementById("result").innerHTML = dialog;
 }
@@ -152,6 +154,7 @@ function showNumberBelow(){
    * HINT: Use the 'warning' and text parameters 
    */
   // *CODE GOES BELOW HERE *
+  let dialog = getDialog("warning", text);
 
   document.getElementById("result").innerHTML = dialog;
 }
